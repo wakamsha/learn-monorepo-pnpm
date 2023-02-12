f3f41555c07a28c7d124e50c9eede9c61e7ee4ea
wakamsha <wakamsha@gmail.com>
2023-02-12 13:33:11 +0900

    :cyclone: app1/home から LabeledSlider を削除する

--- packages/app1/src/templates/home/index.tsx
+++ packages/app1/src/templates/home/index.tsx
@@ -1,37 +1,21 @@
 import { FormLabel } from '@learn-monorepo-pnpm/core/components/inputs/FormLabel';
-import { LabeledSlider } from '@learn-monorepo-pnpm/core/components/inputs/LabeledSlider';
 import { useDebouncedState } from '@learn-monorepo-pnpm/core/hooks/useDebouncedState';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import styles from './index.module.scss';
 
 export const Home = () => {
-  const [weight, setWeight] = useState(60);
-  const [height, setHeight] = useState(170);
-  const calcBMI = useMemo(() => {
-    const heightMeters = height * 0.01;
-    return Math.round(weight / (heightMeters * heightMeters));
-  }, [weight, height]);
-
   const [delay, setDelay] = useState(1000);
   const [value, debouncedValue, setValue] = useDebouncedState('', delay);
 
   return (
     <div className={styles.base}>
-      <h1>hello next.js</h1>
+      <h1>hello next.js app1</h1>
 
       <hr />
 
       <FormLabel label="メールアドレス">
         <input type="email" placeholder="taro.ringo@example.com" />
       </FormLabel>
-
-      <hr />
-
-      <div>
-        <LabeledSlider label="Weight" unit="kg" min={40} max={150} value={weight} onValueChange={setWeight} />
-        <LabeledSlider label="Height" unit="cm" min={140} max={220} value={height} onValueChange={setHeight} />
-        <p>BMI: {calcBMI}</p>
-      </div>
 
       <hr />
 

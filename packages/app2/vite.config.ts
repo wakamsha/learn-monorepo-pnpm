88280ec904aa0c083693013101c287f95fc8bb06
wakamsha <wakamsha@gmail.com>
2023-02-22 17:00:10 +0900

    :cyclone: format

--- packages/app2/vite.config.ts
+++ packages/app2/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
